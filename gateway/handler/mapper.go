7428d0b15225d27d1cea14a16e9185a3e827d689
Javier <sjavierlopez@gmail.com>
2024-03-25 22:57:02 -0600

    feat: define schema for return cluster data

---
+++ gateway/handler/mapper.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"github.com/koor-tech/genesis/gateway/schemas"
+	"github.com/koor-tech/genesis/pkg/models"
+)
+
+func mapCluster(koorCluster *models.Cluster) *schemas.Cluster {
+	schema := schemas.Cluster{
+		ID:    koorCluster.ID,
+		Phase: int(koorCluster.ClusterState.Phase),
+		Customer: schemas.Customer{
+			ID:    koorCluster.Customer.ID,
+			Name:  koorCluster.Customer.Name,
+			Email: koorCluster.Customer.Email,
+		},
+		Provider: schemas.Provider{
+			ID:   koorCluster.Provider.ID,
+			Name: koorCluster.Provider.Name,
+		},
+	}
+
+	if koorCluster.KubeConfig != nil {
+		schema.KubeConfig = koorCluster.KubeConfig
+	}
+	return &schema
+}
